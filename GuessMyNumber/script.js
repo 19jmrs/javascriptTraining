2fa2885bdcb689a27857538bed75f7f5dab54c12
Jorge <jorgesa.work@gmail.com>
2023-11-22 16:38:21 +0000

    Page design and styles

---
+++ GuessMyNumber/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* TODO
+Build a gues my number with retro style
+Guess a number between 20 and 1
+Score goes from 20 to 1 (reduces everytime we error)
+Store the Higherscore on the section
+If the users get's it right, change screen to green
+If try again is pressed the users should be able to start again but the highscore stays the same
+*/
+
